b062ace4223bdb5c9ecfd14b263c56a8f9d4b360
jaehyunlee <jaehyunlee25@gmail.com>
2023-03-27 16:55:15 +0900

    'renew'

--- index.js
+++ index.js
@@ -7,42 +7,41 @@ const log = console.log;
 
 log("start");
 async function start() {
-	log("function", "start");
-	let options = new chrome.Options();
-    options.setChromeBinaryPath(chromium.path);
-    options.addArguments('--headless');
-//    options.addArguments('--disable-gpu');
-    options.addArguments('--no-sandbox');
-//    options.addArguments('--single-process');
-    options.addArguments('--disable-dev-shm-usage');
-//    options.addArguments('--window-size=1280,960');
-	
-	log("driver");
-    try {
+  log("function", "start");
+  let options = new chrome.Options();
+  options.setChromeBinaryPath(chromium.path);
+  options.addArguments("--headless");
+  //    options.addArguments('--disable-gpu');
+  options.addArguments("--no-sandbox");
+  //    options.addArguments('--single-process');
+  options.addArguments("--disable-dev-shm-usage");
+  //    options.addArguments('--window-size=1280,960');
 
-    } catch(e) {
-}
+  log("driver");
+  try {
     const driver = await new webdriver.Builder()
-        .forBrowser('chrome')
-        .setChromeOptions(options)
-        .build();
+      .forBrowser("chrome")
+      .setChromeOptions(options)
+      .build();
 
-	log("call web");
-    await driver.get('http://google.com');
-    console.log('Hello Google!');
-    await takeScreenshot(driver, 'google-start-page');
-    
+    log("call web");
+    await driver.get("http://google.com");
+    console.log("Hello Google!");
+    await takeScreenshot(driver, "google-start-page");
+
     await driver.quit();
+  } catch (e) {
+    log(e);
+  }
 }
 
 async function takeScreenshot(driver, name) {
-	await driver.takeScreenshot().then((data) => {
-        fs.writeFileSync(name + '.png', data, 'base64');
-        console.log('Screenshot is saved');
-    });
+  await driver.takeScreenshot().then((data) => {
+    fs.writeFileSync(name + ".png", data, "base64");
+    console.log("Screenshot is saved");
+  });
 }
 
 const t = setInterval(() => {
-	start();
+  start();
 }, 1000);
-
