cfe731c4dee52f32ae9935337fc0d0d241b2b2ea
jaehyunlee <jaehyunlee25@gmail.com>
2023-03-30 16:11:38 +0900

    'renew'

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const puppeteer = require("puppeteer");
 
 async function main() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto("https://google.com");
 }
