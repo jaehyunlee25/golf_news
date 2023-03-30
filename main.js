36e08fe14922947453eaadaafbf359b32c9a248a
jaehyunlee <jaehyunlee25@gmail.com>
2023-03-30 16:36:41 +0900

    'renew'

--- main.js
+++ main.js
@@ -5,6 +5,15 @@ async function main() {
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
+  page.on("dialog", async (dialog) => {
+    //get alert message
+    console.log(dialog.message());
+    //accept alert
+    await dialog.accept(); //message(), type(),  dismiss(), type()
+  });
+  page.on("console", (msg) => {
+    console.log(msg);
+  });
   await page.goto("https://www.yna.co.kr/sports/golf");
   console.log(page);
   /* const content = await page.content();
